cf1ee1813a4537e952c68684ff65a9964a63f4a9
Alexsey <alexsey.websites.work@gmail.com>
2026-09-07 19:01:55 +0300

    Add files via upload

---
+++ config.h
@@ -0,0 +1,27 @@
+#ifndef FUTURA_XORRISO_CONFIG_H
+#define FUTURA_XORRISO_CONFIG_H
+#define PACKAGE "xorriso"
+#define PACKAGE_NAME "GNU xorriso"
+#define PACKAGE_TARNAME "xorriso"
+#define PACKAGE_VERSION "1.5.8"
+#define PACKAGE_STRING "GNU xorriso 1.5.8"
+#define VERSION "1.5.8"
+#define PACKAGE_BUGREPORT ""
+#define PACKAGE_URL ""
+#define STDC_HEADERS 1
+#define HAVE_STDINT_H 1
+#define HAVE_INTTYPES_H 1
+#define HAVE_STDLIB_H 1
+#define HAVE_STRING_H 1
+#define HAVE_STRINGS_H 1
+#define HAVE_UNISTD_H 1
+#define HAVE_SYS_TYPES_H 1
+#define HAVE_SYS_STAT_H 1
+#define HAVE_SYS_SELECT_H 1
+#define HAVE_POLL_H 1
+#define HAVE_FSEEKO 1
+#undef HAVE_TM_GMTOFF
+#define Libburn_os_has_statvfS 1
+#define Libburn_use_sg_dummY 1
+#define Xorriso_standalonE 1
+#endif
